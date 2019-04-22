a2bf239651fc908617a577eea2fde6dfcd3f954b
xchanin <me@shannonbruns.com>
2019-04-22 14:21:49 -0600

    update title

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -12,7 +12,7 @@ import { distinctUntilChanged } from 'rxjs/operators';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'demo';
+  title = 'lcu-select demo';
 
 
   /**
