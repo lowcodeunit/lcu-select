de87ff1cb8dac73bf3a8333f2102decb6f967952
xchanin <me@shannonbruns.com>
2019-04-30 10:15:08 -0600

    set default value when data is not an array

--- projects/common/src/lib/lcu-select.module.ts
+++ projects/common/src/lib/lcu-select.module.ts
@@ -4,16 +4,17 @@ import { MaterialModule } from './modules/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SelectComponent } from './controls/select/select.component';
+import { CustomMatFormFieldComponent } from './controls/custom-mat-form-field/custom-mat-form-field.component';
 
 @NgModule({
-  declarations: [SelectComponent],
+  declarations: [SelectComponent, CustomMatFormFieldComponent],
   imports: [
     CommonModule,
     MaterialModule,
     ReactiveFormsModule,
     BrowserAnimationsModule
   ],
-  exports: [MaterialModule, SelectComponent],
-  entryComponents: [SelectComponent]
+  exports: [MaterialModule, SelectComponent, CustomMatFormFieldComponent],
+  entryComponents: [SelectComponent, CustomMatFormFieldComponent]
 })
 export class LcuSelectModule { }
