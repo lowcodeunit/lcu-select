ef1056d41b6bd004de96771dbfa6e289e3de889e
xchanin <me@shannonbruns.com>
2019-06-03 09:52:58 -0600

    removed browsermodule and browseranimationmodule from material module

--- projects/common/src/lib/lcu-select.module.ts
+++ projects/common/src/lib/lcu-select.module.ts
@@ -5,16 +5,17 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SelectComponent } from './controls/select/select.component';
 import { CustomMatFormFieldComponent } from './controls/custom-mat-form-field/custom-mat-form-field.component';
+// import { MaterialModule } from '@lcu-ide/common/lib/modules/material.module';
 
 @NgModule({
   declarations: [SelectComponent, CustomMatFormFieldComponent],
   imports: [
     CommonModule,
-    MaterialModule,
     ReactiveFormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
-  exports: [MaterialModule, SelectComponent, CustomMatFormFieldComponent],
+  exports: [SelectComponent, CustomMatFormFieldComponent, MaterialModule],
   entryComponents: [SelectComponent, CustomMatFormFieldComponent]
 })
 export class LcuSelectModule { }
