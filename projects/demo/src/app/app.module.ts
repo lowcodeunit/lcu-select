bd39e9e59f1e159254e85d419389f358cd5312f4
xchanin <me@shannonbruns.com>
2020-08-25 12:53:51 -0600

    upgrade to angular 10

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,14 +1,12 @@
+import { LcuSelectModule } from '@lowcodeunit/lcu-select-common';
+import { FathymSharedModule } from '@lcu/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AgmCoreModule } from '@agm/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LcuSelectModule } from '@lowcodeunit/lcu-select-common';
 import { ReactiveFormsModule } from '@angular/forms';
-// import { MatMenuModule } from '@angular/material';
-import { FathymSharedModule } from '@lcu-ide/common';
-import { MaterialModule } from '@lcu-ide/common/lib/modules/material.module';
 
 @NgModule({
   declarations: [
